fd1d94e18e6f779dce52b90dc71d777e271e60bc
Groza Dominic <groza.dominic@gmail.com>
2022-10-26 12:32:05 +0300

    First commit

---
+++ src/main/java/com/example/ds2022_30433_groza_dominic_1/Ds202230433GrozaDominic1Application.java
@@ -0,0 +1,13 @@
+package com.example.ds2022_30433_groza_dominic_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds202230433GrozaDominic1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ds202230433GrozaDominic1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ds2022_30433_groza_dominic_1/Ds202230433GrozaDominic1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ds2022_30433_groza_dominic_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds202230433GrozaDominic1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
